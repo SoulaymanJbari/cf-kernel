9930b99874fd05ff4080a8c31636592bf26463e7
soulaymanJbari <soulayman.jbari@gmail.com>
2026-07-27 15:46:03 +0200

    Remove (Not working) custom kswapd mechanism

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1787,9 +1787,7 @@ static unsigned long isolate_lru_pages(unsigned long nr_to_scan,
 		nr_pages = compound_nr(page);
 		total_scan += nr_pages;
 
-		if (page_zonenum(page) > sc->reclaim_idx ||
-				skip_cma(page, sc) ||
-			(sc->reclaim_idx == ZONE_LAR && page_zonenum(page) != ZONE_LAR)) {
+		if (page_zonenum(page) > sc->reclaim_idx || skip_cma(page, sc)) {
 			list_move(&page->lru, &pages_skipped);
 			nr_skipped[page_zonenum(page)] += nr_pages;
 			continue;
@@ -6341,10 +6339,6 @@ static bool pgdat_balanced(pg_data_t *pgdat, int order, int highest_zoneidx)
 	unsigned long mark = -1;
 	struct zone *zone;
 
-	if (highest_zoneidx == ZONE_LAR) {
-		return false;
-	}
-
 	/*
 	 * Check watermarks bottom-up as lower zones are more likely to
 	 * meet watermarks.
@@ -6440,10 +6434,6 @@ static bool kswapd_shrink_node(pg_data_t *pgdat,
 			continue;
 
 		sc->nr_to_reclaim += max(high_wmark_pages(zone), SWAP_CLUSTER_MAX);
-	}
-
-	if (sc->reclaim_idx == ZONE_LAR) {
-		sc->nr_to_reclaim = high_wmark_pages(zone);
 	}
 
 	/*
@@ -6936,14 +6926,6 @@ void wakeup_kswapd(struct zone *zone, gfp_t gfp_flags, int order,
 
 	pgdat = zone->zone_pgdat;
 	curr_idx = READ_ONCE(pgdat->kswapd_highest_zoneidx);
-
-	if (highest_zoneidx == ZONE_LAR) {
-		WRITE_ONCE(pgdat->kswapd_highest_zoneidx, ZONE_LAR);
-	} else if(curr_idx == ZONE_LAR) {
-		WRITE_ONCE(pgdat->kswapd_highest_zoneidx, highest_zoneidx);
-	} else if (curr_idx == MAX_NR_ZONES || curr_idx < highest_zoneidx) {
-		WRITE_ONCE(pgdat->kswapd_highest_zoneidx, highest_zoneidx);
-	}
 
 	if (READ_ONCE(pgdat->kswapd_order) < order)
 		WRITE_ONCE(pgdat->kswapd_order, order);
